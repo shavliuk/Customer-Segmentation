8f02b35b3917227abde4ac448c1f209a13ef5937
shavliuk <66632536+shavliuk@users.noreply.github.com>
2020-06-09 09:38:23 +0300

    Add files via upload

---
+++ Customer Segmentation.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+dataset=pd.read_csv('CLV.csv')
+
+print(dataset.head()) #top 5 columns
+len(dataset) # number of rows
+
+#descriptive statistics of the dataset
+dataset.describe().transpose()
+
+X = dataset.copy()
+X = np.array(X)
+
+#Using the elbow method to find the optimum number of clusters
+wcss = []
+for i in range(1,11):
+    km=KMeans(n_clusters=i,init='k-means++', max_iter=300, n_init=10, random_state=0)
+    km.fit(X)
+    wcss.append(km.inertia_)
+plt.plot(range(1,11),wcss)
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('wcss')
+plt.show()
+
+# Based on the elbow plot, we could choose 4,5 or 6 clusters
+
+##Fitting kmeans to the dataset with k=4
+km4=KMeans(n_clusters=4,init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_means = km4.fit_predict(X)
+#Visualizing the clusters for k=4
+plt.scatter(X[y_means==0,0],X[y_means==0,1],s=50, c='purple',label='Cluster1')
+plt.scatter(X[y_means==1,0],X[y_means==1,1],s=50, c='blue',label='Cluster2')
+plt.scatter(X[y_means==2,0],X[y_means==2,1],s=50, c='green',label='Cluster3')
+plt.scatter(X[y_means==3,0],X[y_means==3,1],s=50, c='cyan',label='Cluster4')
+plt.scatter(km4.cluster_centers_[:,0], km4.cluster_centers_[:,1],s=200,marker='s', c='red', alpha=0.7, label='Centroids')
+plt.title('Customer segments')
+plt.xlabel('Annual income of customer')
+plt.ylabel('Annual spend from customer on site')
+plt.legend()
+plt.show()
+
+##Fitting kmeans to the dataset with k=5
+km5=KMeans(n_clusters=5,init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_means = km5.fit_predict(X)
+#Visualizing the clusters for k=4
+plt.scatter(X[y_means==0,0],X[y_means==0,1],s=50, c='purple',label='Cluster1')
+plt.scatter(X[y_means==1,0],X[y_means==1,1],s=50, c='blue',label='Cluster2')
+plt.scatter(X[y_means==2,0],X[y_means==2,1],s=50, c='green',label='Cluster3')
+plt.scatter(X[y_means==3,0],X[y_means==3,1],s=50, c='cyan',label='Cluster4')
+plt.scatter(X[y_means==4,0],X[y_means==4,1],s=50, c='magenta',label='Cluster5')
+plt.scatter(km5.cluster_centers_[:,0], km5.cluster_centers_[:,1],s=200,marker='s', c='red', alpha=0.7, label='Centroids')
+plt.title('Customer segments')
+plt.xlabel('Annual income of customer')
+plt.ylabel('Annual spend from customer on site')
+plt.legend()
+plt.show()
+
+##Fitting kmeans to the dataset - k=6
+km6=KMeans(n_clusters=6,init='k-means++', max_iter=300, n_init=10, random_state=0)
+y_means = km6.fit_predict(X)
+#Visualizing the clusters for k=6
+plt.scatter(X[y_means==0,0],X[y_means==0,1],s=50, c='purple',label='Cluster1')
+plt.scatter(X[y_means==1,0],X[y_means==1,1],s=50, c='blue',label='Cluster2')
+plt.scatter(X[y_means==2,0],X[y_means==2,1],s=50, c='green',label='Cluster3')
+plt.scatter(X[y_means==3,0],X[y_means==3,1],s=50, c='cyan',label='Cluster4')
+plt.scatter(X[y_means==4,0],X[y_means==4,1],s=50, c='magenta',label='Cluster5')
+plt.scatter(X[y_means==5,0],X[y_means==5,1],s=50, c='orange',label='Cluster6')
+plt.scatter(km6.cluster_centers_[:,0], km6.cluster_centers_[:,1],s=200,marker='s', c='red', alpha=0.7, label='Centroids')
+plt.title('Customer segments')
+plt.xlabel('Annual income of customer')
+plt.ylabel('Annual spend from customer on site')
+plt.legend()
+plt.show()
